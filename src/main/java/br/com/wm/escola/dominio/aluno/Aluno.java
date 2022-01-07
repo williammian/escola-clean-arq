92477e87f35db2b854b9339cec047416dd16b4ce
William Mian <william_mian@yahoo.com.br>
2022-01-07 16:33:06 -0300

    autenticacao do aluno

--- src/main/java/br/com/wm/escola/dominio/aluno/Aluno.java
+++ src/main/java/br/com/wm/escola/dominio/aluno/Aluno.java
@@ -12,6 +12,8 @@ public class Aluno {
 	
 	private List<Telefone> telefones = new ArrayList<>();
 	
+	private String senha;
+	
 	public Aluno(CPF cpf, String nome, Email email) {
 		this.cpf = cpf;
 		this.nome = nome;
