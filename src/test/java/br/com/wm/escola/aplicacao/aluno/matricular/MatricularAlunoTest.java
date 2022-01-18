5079b9da4e5bacc80d4961dea9f7055d84e0da87
William Mian <william_mian@yahoo.com.br>
2022-01-18 10:18:19 -0300

    testando o use case

---
+++ src/test/java/br/com/wm/escola/aplicacao/aluno/matricular/MatricularAlunoTest.java
@@ -0,0 +1,28 @@
+package br.com.wm.escola.aplicacao.aluno.matricular;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import br.com.wm.escola.dominio.aluno.Aluno;
+import br.com.wm.escola.dominio.aluno.CPF;
+import br.com.wm.escola.infra.aluno.RepositorioDeAlunosEmMemoria;
+
+class MatricularAlunoTest {
+
+	@Test
+	void alunoDeveriaSerPersistido() {
+		RepositorioDeAlunosEmMemoria repositorio = new RepositorioDeAlunosEmMemoria();
+		MatricularAluno useCase = new MatricularAluno(repositorio);
+		
+		MatricularAlunoDto dados = new MatricularAlunoDto("Fulano", "123.456.789-00", "fulano@email.com");
+		useCase.executa(dados);
+		
+		Aluno encontrado = repositorio.buscarPorCPF(new CPF("123.456.789-00"));
+		
+		assertEquals("Fulano", encontrado.getNome());
+		assertEquals("123.456.789-00", encontrado.getCpf());
+		assertEquals("fulano@email.com", encontrado.getEmail());
+	}
+
+}
